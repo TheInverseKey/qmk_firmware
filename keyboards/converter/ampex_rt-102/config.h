08d0d6b480d6b738385b2195d01e4b815f7a75b4
TheInverseKey <msharkey99@gmail.com>
2026-08-05 22:06:57 -0400

    remove dupe

--- keyboards/converter/ampex_rt-102/config.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include "config_common.h"
-
-#define PRODUCT           Ampex RT 102 keyboard converter
-                          
-#define MATRIX_ROWS       8
-#define MATRIX_COLS       13
